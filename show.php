881052b6e6fbb5e0b62a3d925d38e86f79bbc00d
hepplerj <jason.heppler@gmail.com>
2016-04-25 10:47:09 -0700

    Fixed minor bug with historic map being displayed first.

--- show.php
+++ show.php
@@ -14,18 +14,24 @@
   <div class="btn-group" data-toggle="buttons">
 
     <div class="btn btn-primary active">
-      <input type="radio" name="layer" value="ortelius" checked> Ortelius
+      <input type="radio" name="layer" value="base" checked>Basemap
     </div>
 
     <div class="btn btn-primary">
-      <input type="radio" name="layer" value="kaerio"> Kaerio
+      <input type="radio" name="layer" value="ortelius"> Ortelius
     </div>
 
     <div class="btn btn-primary">
+      <input type="radio" name="layer" value="keere"> Keere
+    </div>
+
+    <div class="btn btn-primary" id="confessions_button">
       <input type="radio" name="layer" value="confessions"> Confessions
     </div>
 
   </div>
 </div>
+
+<img id="legend" src="http://cestastanford.org/projects/rhine/files/original/7acd232867017f3dbbefabca40fc1048.png" alt="Legend">
 
 <?php echo foot(); ?>
